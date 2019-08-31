c1ebdf8c0f156e2e305d4ac817517a2123d50fc1
Carlos Maceira <carlosmgc2003@gmail.com>
2019-08-31 12:54:44 -0300

    Ejercicio 2 Resuelto

---
+++ ejercicio2.py
@@ -0,0 +1,15 @@
+def factorial_recursivo(numero: int) -> int:
+    if numero > 0:
+        return numero * factorial_recursivo(numero - 1)
+    else:
+        return 1
+
+print(factorial_recursivo(8))
+
+def factorial_iterativo(numero: int) -> int:
+    resultado = 1
+    for n in range(1,numero + 1):
+        resultado = resultado * n
+    return resultado
+
+print(factorial_iterativo(8))
