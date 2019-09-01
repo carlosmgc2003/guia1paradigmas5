ea0f0db866a09e6b5e3604c936f116cdc0d4c78e
carlosmgc2003 <36598117+carlosmgc2003@users.noreply.github.com>
2019-09-01 11:50:03 -0300

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,6 +1,13 @@
+#Escribir un programa que reciba como parametro un string de elementtos separados por coma y retorne
+#una lista conteniendo cada elemento.
+
 def formatear_lista(lista : list) -> list:
     return list(map(lista_from_string, lista))
 
+
+#Modificar la funcion anterior pero obteniendo correctamente cada dato segun su tipo, dado el siguiente
+#orden int, string, bool.
+#Modificar para que dada una lista de strings devuelva otra lista, pero con los campos formateados.
 def lista_from_string(string : str) -> list:
     lista = list(string.split(','))
     return [int(lista[0]), lista[1], bool(lista[2])]
