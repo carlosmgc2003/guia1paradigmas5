51650a265db1ddd2010cb2e83a6b9639b97a75de
carlosmgc2003 <36598117+carlosmgc2003@users.noreply.github.com>
2019-09-01 11:54:58 -0300

    Separado implementacion de comprobacion

--- ejercicio4.py
+++ ejercicio4.py
@@ -12,13 +12,15 @@ def lista_from_string(string : str) -> list:
     lista = list(string.split(','))
     return [int(lista[0]), lista[1], bool(lista[2])]
 
-ejemplo = [
-            '14,Juana Perez,True',
-            '16,Raul Dell,False',
-            '18,Mariana Castillo,True',
-            '176,Pedro Rodriguez,False'
-            ]
 
-print(lista_from_string(ejemplo[0]))
+if __name__ == "__main__":
+    ejemplo = [
+                '14,Juana Perez,True',
+                '16,Raul Dell,False',
+                '18,Mariana Castillo,True',
+                '176,Pedro Rodriguez,False'
+                ]
 
-print(formatear_lista(ejemplo))
+    print(lista_from_string(ejemplo[0]))
+
+    print(formatear_lista(ejemplo))
